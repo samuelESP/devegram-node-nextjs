ee402412000c638ef7cece64a8a1aae22f3c37a4
samuelESP <samuelnegocios1509@gmail.com>
2024-04-16 23:04:36 -0300

    corrigindo conflitos

--- pages/api/cadastro.ts
+++ pages/api/cadastro.ts
@@ -4,13 +4,15 @@ import {conectaMongoDB} from "../../middlewares/conectaMongoDB";
 import type {respostaPadraoMsg} from "../../types/respostaPadraoMsg";
 import {UserModel} from "../../models/UserModel";
 import md5 from 'md5';
+import {upload, uploadCosmicImagens} from "../../services/UploadCosmicImagens"
+import nc from "next-connect";
 
-
-const  endpointCadastro = 
-async (req: NextApiRequest, res:NextApiResponse<respostaPadraoMsg>) => {
-    if(req.method === "POST"){
+const handler = nc()
+    .use(upload.single("file"))
+    .post(async (req: NextApiRequest, res:NextApiResponse<respostaPadraoMsg>) => {
+    try {
         const user = req.body as CadastroRequisicao;
-
+                
         if(!user.nome || user.nome.length < 2){
             return res.status(400).json({erro: "Nome inválido"})
         }
@@ -20,21 +22,35 @@ async (req: NextApiRequest, res:NextApiResponse<respostaPadraoMsg>) => {
         if(!user.password || user.password.length < 4){
             return res.status(400).json({erro: "senha inválido"})
         }
-        // validação, se já existe user com o mesmo email
+    // validação, se já existe user com o mesmo email
         const usersWithSameEmail = await UserModel.find({email: user.email})
-        if(usersWithSameEmail && usersWithSameEmail.length > 0){
+            if(usersWithSameEmail && usersWithSameEmail.length > 0){
             return res.status(400).json({erro: "Usuário já existente"})
-        }
+            }
+            
+        // Enviar a imagen do multer que já foi processada, para o cosmic
+        const image = await uploadCosmicImagens(req); 
         // Salvar no banco de dados
         const userCriptografado = {
             nome: user.nome,
             email: user.email,
-            password: md5(user.password)
+            password: md5(user.password),
+            avatar: image?.media?.url,
         }
         await UserModel.create(userCriptografado)
         return res.status(200).json({msg: "Usuario criado com sucesso"})
-    }
-    return res.status(405).json({erro: "Método informado não é válido"})
-}
+    }catch (e) {  
+        console.log(`${e}`);
+        
+        return res.status(500).json({erro: `Erro ao cadastrar usuário`})
+        
+    }})
+// Configurando para que o NEXT não mande uma request com um JSON, por estarmos enviando uma foto é necessário que seja um FormData
+export const config = { 
+    api: {
+      bodyParser: false,
+    },
+  };
 
-export default conectaMongoDB(endpointCadastro);
+
+export default conectaMongoDB(handler);
